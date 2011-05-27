53b0d1aa658452622d9c44710b5e8b424735ddc8
Jonathan Leung <jonleung@seas.upenn.edu>
2011-05-27 00:52:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd01168e6846f3ee40ffceaf2f4296b3c5f164650eb63ca22740d6ed6b24ac4a211197823e33339eb236857fa0577980a2002e883f08906ad5a7edfb1d97d6bbe'
